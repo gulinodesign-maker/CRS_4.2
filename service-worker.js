0c027ea2beb63856cfe4d8cb1920c84f9c98d4f2
gulinodesign-maker <gulinodesign@gmail.com>
2025-12-22 23:12:47 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "CRS-4.72";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./crs-icon-192.png",
+  "./crs-icon-512.png",
+  "./crs-icon.png",
+  "./BANNER.jpg"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES_TO_CACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  const url = new URL(e.request.url);
+  if (url.hostname.includes("script.google.com")) return;
+
+  if (e.request.mode === "navigate") {
+    e.respondWith(
+      fetch(e.request, { cache: "no-store" })
+        .then(r => {
+          const c = r.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put("./index.html", c));
+          return r;
+        })
+        .catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
