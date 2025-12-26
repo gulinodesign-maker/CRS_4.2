f00f34ce604ce036e549dbc28327fe685503a265
gulinodesign-maker <gulinodesign@gmail.com>
2025-12-26 19:54:53 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-/* CRS_4.144 */
+/* CRS_4.145 */
 'use strict';
 
-const BUILD = 'CRS_4.144';
+const BUILD = 'CRS_4.145';
 const CACHE_NAME = `crs-cache-${BUILD}`;
 const HTML_CACHE = `crs-html-${BUILD}`;
 const ASSET_CACHE = `crs-assets-${BUILD}`;
@@ -11,6 +11,8 @@ const CORE_ASSETS = [
   './',
   './index.html',
   './manifest.json',
+  './bg-track.jpg',
+  './crs-icon.png',
   './icons/icon-192.png',
   './icons/icon-512.png'
 ];
